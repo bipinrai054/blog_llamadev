255d7f328b5441cf10b988d8a01acd15c42a0b0e
bipnrai054 <bipinrai054@gmail.com>
2022-10-04 23:36:16 +0545

    single post fetched from database

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -8,53 +8,67 @@ import Delete from '../img/delete.png';
 import { Link } from 'react-router-dom';
 import Menu from '../components/Menu';
 
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment';
+
+import { AuthContext } from '../context/authContext';
+
 export default function Single() {
+  const [post, setPost] = React.useState({});
+
+  const location = useLocation();
+
+  const postId = location.pathname.split('/')[2];
+
+  const { currentUser } = React.useContext(AuthContext);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
   return (
     <div className='single'>
       <div className='content'>
+        {console.log(post)}
         <img
-          src='https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'
-          alt='test'
+          // src='https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'
+          src={post?.img}
+          alt='nioce'
         />
 
         <div className='user'>
-          <img
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png'
-            alt='test'
-          />
+          {post.userImg && (
+            <img
+              // src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png'
+              src={post.userImg}
+              alt='test'
+            />
+          )}
           <div className='info'>
-            <span>John</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className='edit'>
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt='edit' />
-            </Link>
-            <img src={Delete} alt='delete' />
-          </div>
+
+          {currentUser?.username === post.username && (
+            <div className='edit'>
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt='edit' />
+              </Link>
+              <img src={Delete} alt='delete' />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur adip alet.</h1>
-        <p>
-          of letters, as opposed to using 'Content here, content here', making
-          it look like readable English. Many desktop
-          <br /> publishing packages and web page editors now use Lorem Ipsum as
-          their default model text, and a search for 'lorem ipsum' will
-          <br /> uncover many web sites still in their infancy. Various versions
-          have evolved over the years, sometimes by accident, sometimes on
-          purpose (injected humour and the like). There are many variations of
-          passages of Lorem Ipsum available, but the majority have suffered
-          alteration in some form, by injected humour, or randomised words which
-          don't look even slightly believable. If you are going to use a passage
-          of Lorem Ipsum, you need to be sure there isn't anything embarrassing
-          hidden in the middle of text. All th
-          <br /> e Lorem Ipsum generators on the Internet tend to repeat
-          predefined chunks as necessary, making this the first true generator
-          on the Internet. It uses a dictionary of over 200 Latin words,
-          combined with a handful of model sentence structures, to generate
-          Lorem Ipsum which looks reasonable. <br />
-          The generated Lorem Ipsum is therefore always free from repetition,
-          injected humour, or non-characteristic words etc.
-        </p>
+        <h1>{post.title}</h1>
+        {post.description}
       </div>
       <Menu />
     </div>
