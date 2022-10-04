bb1a3d6278c5b3c68004335d536c9fea9a2b6ea3
bipnrai054 <bipinrai054@gmail.com>
2022-10-04 14:44:17 +0545

    routes and controllers setup done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 
+// routes
+import postRoutes from './routes/posts.js';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/posts', postRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => res.json('noice'));
 
 app.listen(5000, () => console.log('Server running'));
