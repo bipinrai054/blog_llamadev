751997c9a9bd4890a290327d5e9e4e873ec69b66
bipnrai054 <bipinrai054@gmail.com>
2022-10-05 19:10:02 +0545

    new post adding functionality added in client

--- api/index.js
+++ api/index.js
@@ -7,10 +7,29 @@ import userRoutes from './routes/users.js';
 
 import cookieParser from 'cookie-parser';
 
+import multer from 'multer';
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// multer for file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../client/public/upload');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', authRoutes);
